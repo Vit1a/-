f5a16b55a30d5ac931eb23fc74634f76e98ee898
Vit1a <hydrogen.idkhowtonameit@gmail.com>
2026-03-29 14:49:03 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 from customtkinter import *
 
-HOST = '192.168.0.113'
+HOST = '192.168.0.102'
 PORT = 12345
 
 clients = []
@@ -12,9 +12,9 @@ def broadcast(data, exclude_socket=None):
         if client != exclude_socket:
             try:
                 client.sendall(data)
-            expect:
-            pass
-        
+            except:
+                pass
+
 def handle_client(client_socket):
     while True:
         try:
@@ -24,10 +24,10 @@ def handle_client(client_socket):
             broadcast(data, exclude_socket=client_socket)
         except:
             break
+    
     if client_socket in clients:
         clients.remove(client_socket)
     client_socket.close()
-
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,12 +38,12 @@ def main():
 
     while True:
         client_socket, addr = server_socket.accept()
-        print(f"Підлкючився клієнт: {addr}")
+        print(f"Підключився клієнт: {addr}")
         clients.append(client_socket)
 
         t = threading.Thread(target=handle_client, args=(client_socket,))
+        t.daemon = True # Позволит серверу закрыться, даже если потоки активны
         t.start()
-
 
 if __name__ == "__main__":
     main()
